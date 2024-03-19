ff3f5ae687726896627fba9671a86a57adbab34d
Vadim Kuznetsov <darovadraste@gmail.com>
2024-03-19 19:43:42 +0300

    day00 done

---
+++ Go_Day00-1/src/ex00/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"ex00/process"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Input sequence of numbers separeted by newlines. Input 'end' to end the input")
+	nums := []int{}
+	outputTypes := flag.String("type", "not specified", `Input desired output type. 
+	Available variants are: 'Mean', 'Median', 'Mode', 'SD'`)
+	flag.Parse()
+
+	err := process.ParseInput(&nums)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error during parsing: %s\n", err.Error())
+		os.Exit(1)
+	}
+	if len(nums) == 0 {
+		fmt.Fprintf(os.Stderr, "Empty input")
+		os.Exit(1)
+	}
+
+	sort.Ints(nums)
+	var (
+		mean, median, sd float64
+		mode             int
+	)
+
+	mean = process.CalcMean(nums)
+	median = process.CalcMedian(nums)
+	mode = process.CalcMode(nums)
+	sd = process.CalcSd(nums, mean)
+	switch *outputTypes {
+	case "Mean":
+		fmt.Printf("Mean: %.2f\n", mean)
+	case "Median":
+		fmt.Printf("Median: %.2f\n", median)
+	case "Mode":
+		fmt.Println("Mode:", mode)
+	case "SD":
+		fmt.Printf("SD: %.2f\n", sd)
+	default:
+		fmt.Printf("Mean: %.2f\nMedian: %.2f\nMode: %d\nSD: %.2f\n", mean, median, mode, sd)
+	}
+}
---
+++ Go_Day00-1/src/ex00/process/process.go
@@ -0,0 +1,76 @@
+package process
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func ParseInput(nums *[]int) error {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return fmt.Errorf("error occured: %s", err.Error())
+		}
+		line = line[:len(line)-1]
+		if line == "end" {
+			break
+		}
+		elems := strings.Fields(line)
+		for _, val := range elems {
+			num, err := strconv.Atoi(val)
+			if err != nil {
+				return fmt.Errorf("wrong input: %s", err.Error())
+			}
+			*nums = append(*nums, num)
+		}
+	}
+	return nil
+}
+
+func CalcMean(nums []int) (mean float64) {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return float64(sum) / float64(len(nums))
+}
+
+func CalcMedian(nums []int) (median float64) {
+	if len(nums)%2 != 0 {
+		median = float64(nums[len(nums)/2])
+	} else {
+		median = (float64(nums[len(nums)/2]-1) + float64(nums[len(nums)/2])) / 2.0
+	}
+	return
+}
+
+func CalcMode(nums []int) (mode int) {
+	numberCounts := make(map[int]int, len(nums))
+	for _, num := range nums {
+		numberCounts[num]++
+	}
+
+	mode, max_val := slices.Max(nums), numberCounts[nums[0]]
+	for key, val := range numberCounts {
+		if val > max_val {
+			max_val = val
+			if key < mode {
+				mode = key
+			}
+		}
+	}
+	return
+}
+
+func CalcSd(nums []int, mean float64) (sd float64) {
+	for _, num := range nums {
+		sd += math.Pow(float64(num)-mean, 2)
+	}
+	return math.Sqrt(sd / float64(len(nums)-1))
+}
